a639b281bf160a1cd2086c391e2be7451768e6eb
Matthew Starcher <mlstarcher@gmail.com>
2022-03-05 19:45:13 -0800

    First expo publish

--- driving-group/components/Home.js
+++ driving-group/components/Home.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>You're all caught up!</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  welcome: {
+    color: '#888',
+    fontSize: 22,
+    marginHorizontal: 15,
+    marginTop: 50,
+  },
+});
